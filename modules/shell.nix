c8a72fe953e847670e90a34991f1fad7470403d8
Firdaus Rozi <firdausious@gmail.com>
2026-02-19 08:27:06 +0700

    add clear cache & clean

--- modules/shell.nix
+++ modules/shell.nix
@@ -167,7 +167,8 @@
 
     # Nix flake management
     flakeup = "nix flake update ${defaults.nixConfigDirectory} --update-input";
-    nc = "nix flake update nixpkgs-unstable --extra-experimental-features nix-command --extra-experimental-features flakes";
+    ncache = "nix flake update nixpkgs-unstable --extra-experimental-features nix-command --extra-experimental-features flakes";
+    nclean = "nix-collect-garbage --delete-older-than 7d";
     nxb =
       "nix build ${defaults.nixConfigDirectory}/#homeConfigurations.${system}.${username}.activationPackage -o ${defaults.nixConfigDirectory}/result --extra-experimental-features nix-command --extra-experimental-features flakes";
     nxa =
