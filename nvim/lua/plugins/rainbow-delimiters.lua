1eb38e2517bc016485a0367edbf55cbcecce742f
Firdaus Rozi <firdausious@gmail.com>
2026-04-03 19:48:58 +0700

    update libs, add docuseal

--- nvim/lua/plugins/rainbow-delimiters.lua
+++ nvim/lua/plugins/rainbow-delimiters.lua
@@ -5,5 +5,18 @@ return {
     dependencies = "nvim-treesitter/nvim-treesitter",
     event = "User AstroFile",
     main = "rainbow-delimiters.setup",
+    opts = {
+      condition = function(bufnr)
+        local excluded_filetypes = {
+          "notify", "noice", "lazy", "mason", "oil", "fugitive", "alpha"
+        }
+        local filetype = vim.bo[bufnr].filetype
+        if vim.tbl_contains(excluded_filetypes, filetype) then
+          return false
+        end
+        local ok, parser = pcall(vim.treesitter.get_parser, bufnr)
+        return ok and parser ~= nil
+      end,
+    },
   },
 }
